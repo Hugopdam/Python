a072a6bf1fc2874bc64cedb774988e21c961dea0
Hugo ASIR <hugopdam@gmail.com>
2025-09-26 13:28:54 +0200

    actividad1

--- .idea/actividad1.py
+++ .idea/actividad1.py
@@ -1,3 +1,3 @@
 nombre = input("introduce tu nombre:")
 edad= input("Introduce tu edad:")
-print(f"Hola {nombre}, tienes {edad} años. Bienvenida a python" )
+print(f"Hola {nombre}, tienes {edad} años. Bienvenida a python")
