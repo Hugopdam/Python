5d8790ecd945f7c0c99e69da996b4dcd0de59ff1
Hugo ASIR <hugopdam@gmail.com>
2025-09-29 13:43:51 +0200

    subida

--- .idea/ejercicioguiadob1_2.py
+++ .idea/ejercicioguiadob1_2.py
@@ -1,6 +1,5 @@
 nombre = input("introduce tu nombre:")
 edad= input("Introduce tu edad:")
 altura= input("Introduce tu estatura:")
-estudio= input("Esta estudiando?")
-print(f"Hola {nombre}, tienes {edad} años, mides {altura}cm. Etudias:"),print(bool(estudio))
-
+estudio = "True"
+print(f"Hola {nombre}, tienes {edad} años, mides {altura}cm. Etudias: {estudio}")
